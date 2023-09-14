abeebfacc4df9962241699b1ca7041b8abfd004d
victor <victormmla3lm@gmail.com>
2023-09-14 14:19:49 -0300

    new updates!

--- hola-mundo/src/main/java/ar/com/codoacodo/holamundo/controllers/Controller.java
+++ hola-mundo/src/main/java/ar/com/codoacodo/holamundo/controllers/Controller.java
@@ -3,6 +3,7 @@ package ar.com.codoacodo.holamundo.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SuppressWarnings("unused")
 @RestController
 public class Controller {
 
