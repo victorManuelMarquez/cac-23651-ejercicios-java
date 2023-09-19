3632c825124bfb2582921d930c9baf517a415d5f
victor <victormmla3lm@gmail.com>
2023-09-19 12:30:55 -0300

    new updates!

--- links/src/main/java/ar/com/codoacodo/links/controller/Controller.java
+++ links/src/main/java/ar/com/codoacodo/links/controller/Controller.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@SuppressWarnings("unused")
 @RestController
 public class Controller {
 
